93a245a324b873c30210a71816caa4ceea9807c4
Nika <nikadvalishvili517@gmail.com>
2024-10-08 17:41:05 +0400

    made checkout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ProductsComponent } from "./products/products.component";
 import { CouponComponent } from "./coupon/coupon.component";
+import { CheckoutComponent } from "./checkout/checkout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProductsComponent, CouponComponent],
+  imports: [RouterOutlet, HeaderComponent, ProductsComponent, CouponComponent, CheckoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
